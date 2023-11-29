c36f9f45d16faeffe84709f416adfbbc41ddbd00
Joar <crisostomo.mavijoar2003@gmail.com>
2023-11-29 17:56:26 +0800

    Firebase Added and Connected

--- app/src/main/java/com/example/signupui/MainActivity.java
+++ app/src/main/java/com/example/signupui/MainActivity.java
@@ -2,31 +2,32 @@ package com.example.signupui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    TextView loginTv;
+    EditText usernameInput, emailInput, passwordInput, cPasswordInput;
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.sgnbtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SignUp();
-            }
-        });
+        //initialize components
+        usernameInput = findViewById(R.id.usernameEt);
+        emailInput = findViewById(R.id.emailEt);
+        passwordInput = findViewById(R.id.passwordEt);
+        cPasswordInput = findViewById(R.id.cPasswordEt);
+        signUpBtn = findViewById(R.id.signUpBtn);
+
     }
 
     public void SignUp () {
-        Intent intent = new Intent(this, activity2.class);
-        startActivity(intent);
+
     }
 
 }
