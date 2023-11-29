5c5457c4886429e0aae2d6a793c01abdf6277dd3
Joar <crisostomo.mavijoar2003@gmail.com>
2023-11-29 23:00:32 +0800

    Read stored user details

--- app/src/main/java/com/example/signupui/HomeActivity.java
+++ app/src/main/java/com/example/signupui/HomeActivity.java
@@ -1,25 +1,35 @@
 package com.example.signupui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
-    TextView usernameTv, userEmailtv;
-    String username, email;
+    TextView usernameTv, emailtv;
+    String username, email, userId;
     Button logoutBtn;
     UserDataClass userData;
 
     //Firebase Initialization;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private DatabaseReference rootDtb, userDtb;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +37,45 @@ public class HomeActivity extends AppCompatActivity {
 
         //initialize components
         usernameTv = findViewById(R.id.userNameTv);
-        userEmailtv = findViewById(R.id.userEmailTv);
+        emailtv = findViewById(R.id.emailTv);
         logoutBtn = findViewById(R.id.logoutBtn);
 
+        //authentication
         mAuth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
         currentUser = mAuth.getCurrentUser();
         if(currentUser == null){
             gotoLogin();
         }else{
             //display
+            userId = currentUser.getUid();
         }
+
+        //realtime database
+        rootDtb = FirebaseDatabase.getInstance().getReference();
+        userDtb = rootDtb.child("users").child(userId);
+        //read user details
+        userDtb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserDataClass userData = snapshot.getValue(UserDataClass.class);
+                username = userData.getUsername();
+                email = userData.getEmail();
+                usernameTv.setText(username);
+                emailtv.setText(email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomeActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         logoutBtn.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
