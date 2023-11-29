969fd6484fa12c5eeb177cc4b4c784ba411b4626
Joar <crisostomo.mavijoar2003@gmail.com>
2023-11-29 23:38:06 +0800

    Initialize components in LoginActivity.java

--- app/src/main/java/com/example/signupui/LoginActivity.java
+++ app/src/main/java/com/example/signupui/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.signupui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText usernameEt, passwordEt;
+    Button loginBtn;
+    TextView forgotPasswordTv, signupTv;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //initialize component
+        usernameEt = findViewById(R.id.usernameEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        loginBtn = findViewById(R.id.loginBtn);
+        forgotPasswordTv = findViewById(R.id.forgotPasswordTv);
+        signupTv = findViewById(R.id.signupTv);
     }
 }
